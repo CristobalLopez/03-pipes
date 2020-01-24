feb4c576f5ae4de2018793bba1ece352eed021bb
CristobalLopez <clopezosses@gmail.com>
2020-01-24 15:15:48 -0300

    commit hasta pipe domseguro video 82

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { resolve } from 'url';
 
 @Component({
   selector: 'app-root',
@@ -7,15 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   nombre ="Cristobal";
+  nombre2= "cristobaL fabiAn loPez ossEs";
   arreglo = [1,2,3,4,5,6,7,8,9,10];
   PI = Math.PI;
   a:number = 0.234;
   salario: number= 1234.5;
-  heroe= {
+  heroe = {
     nombre: "Logan",
     clave: "Wolverine",
     edad: 500,
+    direccion:{
     calle: "Primera",
     casa: "19"
-  }
+    }
+  };
+
+  valorDePromesa = new Promise((resolve, reject)=>{
+    setTimeout(()=>resolve('Llego la Data!'), 3500);
+  });
+
+  fecha = new Date();
+  video = "9PEdf9zJLPA";
 }
